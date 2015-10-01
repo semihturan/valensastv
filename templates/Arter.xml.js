09519c3fc4103ebf6cd079401d66d43c4feec192
Semih Enver Turan <semihturan@outlook.com>
2015-10-02 00:06:20 +0300

    no message

--- templates/Arter.xml.js
+++ templates/Arter.xml.js
@@ -45,92 +45,22 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
         <lockup>
           <img src="${this.BASEURL}resources/images/arter/6_1.png" width="375" height="667" />
           <title class="showTextOnHighlight">Exhibitions</title>
-          <overlay>
-            <progressBar value="0.1" />
-          </overlay>
-          <relatedContent>
-            <infoTable>
-              <header>
-                <title>Info Header 1</title>
-              </header>
-              <info>
-                <header>
-                  <title>Title 1</title>
-                </header>
-                <description allowsZooming="true">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</description>
-              </info>
-            </infoTable>
-          </relatedContent>
         </lockup>
         <lockup>
           <img src="${this.BASEURL}resources/images/arter/6_2.png" width="375" height="667" />
           <title class="showTextOnHighlight">Explore</title>
-          <relatedContent>
-            <infoTable>
-              <header>
-                <title>Info Header 2</title>
-              </header>
-              <info>
-                <header>
-                  <title>Title 2</title>
-                </header>
-                <text>A line of text</text>
-              </info>
-            </infoTable>
-          </relatedContent>
         </lockup>
         <lockup>
           <img src="${this.BASEURL}resources/images/arter/6_3.png" width="375" height="667" />
           <title class="showTextOnHighlight">Works</title>
-          <relatedContent>
-            <infoTable>
-              <header>
-                <title>Info Header 3</title>
-              </header>
-              <info>
-                <header>
-                  <title>Title 3</title>
-                </header>
-                <description allowsZooming="true">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</description>
-              </info>
-            </infoTable>
-          </relatedContent>
         </lockup>
         <lockup>
           <img src="${this.BASEURL}resources/images/arter/6_4.png" width="375" height="667" />
           <title class="showTextOnHighlight">Publications</title>
-          <relatedContent>
-            <infoTable>
-              <header>
-                <title>Info Header 4</title>
-              </header>
-              <info>
-                <header>
-                  <title>Title 4</title>
-                </header>
-                <row>
-                  <text>A line of text</text>
-                </row>
-              </info>
-            </infoTable>
-          </relatedContent>
         </lockup>
         <lockup>
           <img src="${this.BASEURL}resources/images/arter/6_1.png" width="375" height="667" />
           <title class="showTextOnHighlight">Events</title>
-          <relatedContent>
-            <infoTable>
-              <header>
-                <title>Info Header 5</title>
-              </header>
-              <info>
-                <header>
-                  <title>Title 5</title>
-                </header>
-                <description allowsZooming="true">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</description>
-              </info>
-            </infoTable>
-          </relatedContent>
         </lockup>
       </section>
     </shelf>
