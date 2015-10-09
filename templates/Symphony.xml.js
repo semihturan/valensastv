6a1c1385d0beac6bf031d67ef9ba4274c46c05ad
Semih Enver Turan <semihturan@outlook.com>
2015-10-09 14:13:15 +0300

    no message

--- templates/Symphony.xml.js
+++ templates/Symphony.xml.js
@@ -4,8 +4,7 @@ See LICENSE.txt for this sample’s licensing information
 
 Abstract:
 A product bundle template promotes a series of related TV shows, movies, or other products. It typically includes an image, background, and descriptive information. A shelf below the content displays the products contained by the bundle, such as the episodes of a TV season. The user can scroll down to bring up more information, such as cast and crew listings, ratings, or reviews.
-*/
-var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
+*/var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
 <document>
   <head>
     <style>
@@ -23,15 +22,15 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
     }
     </style>
   </head>
-  <productBundleTemplate class="tintBackgroundColor"> 
+  <productBundleTemplate>
     <background>
-      <img src="${this.BASEURL}resources/images/roche/roche-background.jpg" />
+      <img src="${this.BASEURL}resources/images/arter/roche-background.jpg" />
     </background>
     <banner>
       <stack>
         <title>Roche DIA Symphony</title>
         <subtitle>iOS Client App</subtitle>
-        <description autoHighlight="true" class="justified" allowsZooming="true">In 2015, Valensas Technology has developed Roche DIA Symphony application for Roche DIA. The application gives information about RACI categories of DIA division. Moreover, the app pro- vides a Learning Map, Frequently Asked Questions, a Glossary, documents of Divisional Stan- dards. Application is optimized for iOS 8, and iPhone 6 and 6 Plus. iOS 7 and older devices (iPhone 4 and higher, and iPad 2 and higher) are also supported. Since DIA Symphony application is similar to the previously built Pharma Symphony app, it was developed for testing in just under two weeks.</description>
+        <description class="justified" allowsZooming="true">In 2015, Valensas Technology has developed Roche DIA Symphony application for Roche DIA. The application gives information about RACI categories of DIA division. Moreover, the app pro- vides a Learning Map, Frequently Asked Questions, a Glossary, documents of Divisional Stan- dards. Application is optimized for iOS 8, and iPhone 6 and 6 Plus. iOS 7 and older devices (iPhone 4 and higher, and iPad 2 and higher) are also supported. Since DIA Symphony application is similar to the previously built Pharma Symphony app, it was developed for testing in just under two weeks.</description>
         <row>
           <buttonLockup>
             <badge src="resource://button-rate" class="whiteButton" />
@@ -48,14 +47,6 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
         <lockup>
           <img src="${this.BASEURL}resources/images/roche/6_1.png" width="375" height="667" />
           <title class="showTextOnHighlight">Welcome</title>
-        </lockup>
-        <lockup>
-          <img src="${this.BASEURL}resources/images/roche/6_2.png" width="375" height="667" />
-          <title class="showTextOnHighlight">Menu</title>
-        </lockup>
-        <lockup>
-          <img src="${this.BASEURL}resources/images/roche/6_3.png" width="375" height="667" />
-          <title class="showTextOnHighlight">RACI</title>
         </lockup>
         <lockup>
           <img src="${this.BASEURL}resources/images/roche/6_2.png" width="375" height="667" />
