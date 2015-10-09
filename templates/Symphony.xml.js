a92d98848965fe13afcfc4a39babf612863404cc
Semih Enver Turan <semihturan@outlook.com>
2015-10-09 11:44:37 +0300

    no message

--- templates/Symphony.xml.js
+++ templates/Symphony.xml.js
@@ -34,7 +34,7 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
       <stack>
         <title>Roche DIA Symphony</title>
         <subtitle>iOS Client App</subtitle>
-        <description class="justified" allowsZooming="true">In 2015, Valensas Technology has developed Roche DIA Symphony application for Roche DIA. The application gives information about RACI categories of DIA division. Moreover, the app pro- vides a Learning Map, Frequently Asked Questions, a Glossary, documents of Divisional Stan- dards. Application is optimized for iOS 8, and iPhone 6 and 6 Plus. iOS 7 and older devices (iPhone 4 and higher, and iPad 2 and higher) are also supported. Since DIA Symphony application is similar to the previously built Pharma Symphony app, it was developed for testing in just under two weeks.</description>
+        <description class="justified background" allowsZooming="true">In 2015, Valensas Technology has developed Roche DIA Symphony application for Roche DIA. The application gives information about RACI categories of DIA division. Moreover, the app pro- vides a Learning Map, Frequently Asked Questions, a Glossary, documents of Divisional Stan- dards. Application is optimized for iOS 8, and iPhone 6 and 6 Plus. iOS 7 and older devices (iPhone 4 and higher, and iPad 2 and higher) are also supported. Since DIA Symphony application is similar to the previously built Pharma Symphony app, it was developed for testing in just under two weeks.</description>
         <row>
           <buttonLockup>
             <badge src="resource://button-rate" class="whiteButton" />
